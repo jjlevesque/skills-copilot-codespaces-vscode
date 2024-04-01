302b8a66dd0e16353791c2d652f49dd0ab75d4b3
jjlevesque <121617991+jjlevesque@users.noreply.github.com>
2024-04-01 18:38:33 +0000

    add multiple suggestions

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  return {
+    name: 'skillsMember',
+    path: '/skillsMember',
+    meta: {
+      title: '技能管理',
+      icon: 'el-icon-s-tools',
+      roles: ['admin', 'editor']
+    }
+  }
+}
